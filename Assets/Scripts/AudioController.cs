c020b229b9766a75f9467076badc82c3bd82787e
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2017-01-19 11:40:16 +1030

    Add nearly complete Ptolemy scene

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioController : MonoBehaviour {
+
+    public PlanetController[] planets;
+    public float spectraCutOffRatio;
+    public bool showSpectrum;
+    public Camera tempCamera;
+
+    private AudioSource visualiserAudioSource;
+    private int sampleCount = 8192;
+
+    void Start() {
+        visualiserAudioSource = GetComponent<AudioSource>();
+        visualiserAudioSource.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float[] channelASpectrum = new float[sampleCount];
+        float[] channelBSpectrum = new float[sampleCount];
+        int channelA = 0;
+		int channelB = 1;
+        FFTWindow window = FFTWindow.BlackmanHarris;
+
+        int usefulSampleCount = (int) (spectraCutOffRatio * sampleCount);
+
+        bool useVisualiserAudioSource = false;
+        foreach (PlanetController planet in planets) {
+            if (!planet.isActiveAndEnabled) {
+                continue;
+            }
+
+            // Planets can each have their own AudioSource or just use our own AudioSource
+            AudioSource audioSource = planet.GetComponent<AudioSource>();
+            if (audioSource == null) {
+                audioSource = visualiserAudioSource;
+                useVisualiserAudioSource = true;
+                Debug.Log("Use happy days toy town");
+            }
+
+            audioSource.GetSpectrumData(channelASpectrum, channelA, window);
+            audioSource.GetSpectrumData(channelBSpectrum, channelB, window);
+
+            float audioForce = 0;
+            int frequencyCount = 0;
+
+            List<float> spectrumValues = new List<float>();
+            for (int i = 0; i < usefulSampleCount; i++) {
+                float monoSpectrumValue = channelASpectrum[i] + channelBSpectrum[i];
+
+                float indexRatio = i / (float) usefulSampleCount;
+                if (indexRatio >= planet.frequencyThresholdLowerBound &&
+                    indexRatio <= planet.frequencyThresholdUpperBound) {
+                    frequencyCount++;
+                    audioForce += monoSpectrumValue;
+                    spectrumValues.Add(monoSpectrumValue);
+                }
+            }
+            audioForce /= (float) frequencyCount;
+            planet.setAudioForce(audioForce);
+            planet.setSpectrumValues(spectrumValues);
+        }
+
+        if (useVisualiserAudioSource) {
+            visualiserAudioSource.enabled = true;
+        }
+
+        if (showSpectrum) {
+            Vector3[] spectrumPoints = new Vector3[usefulSampleCount];
+            float width = 10;
+            for (int i = 0; i < usefulSampleCount; i++) {
+                float monoSpectrumValue = channelASpectrum [i] + channelBSpectrum [i];
+                Vector3 point = spectrumPoints [i];
+                point.x = tempCamera.transform.position.x - width / 2 + i * width / (float)usefulSampleCount;
+                point.y = monoSpectrumValue * 20;
+                point.z = tempCamera.transform.position.z + 5;
+                spectrumPoints [i] = point;
+            }
+            GetComponent<LineRenderer>().SetVertexCount(usefulSampleCount);
+            GetComponent<LineRenderer>().SetPositions(spectrumPoints);
+        } else {
+            GetComponent<LineRenderer>().SetVertexCount(0);
+        }
+    }
+}
---
+++ Assets/Scripts/PlanetController.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PlanetController : MonoBehaviour {
+
+    public float deferentRadius;
+    public float epicycleRadius;
+    public float deferentSpeed;
+    public float epicycleSpeed;
+    public LineRenderer epicycleRenderer;
+    public LineRenderer deferentRenderer;
+    public Camera camera;
+
+    public float frequencyThresholdLowerBound;
+    public float frequencyThresholdUpperBound;
+    public float audioForcePreExponentMultiplier;
+    public float audioForceExponent;
+    public float audioForcePostExponentMultipiler;
+    public float visualPreExponentMultiplier;
+    public float visualExponent;
+    public Color colour;
+    public Text text;
+
+    private Vector3 cameraOffset;
+    private float totalDeferentTravel = 0;
+    private float totalEpicycleTravel = 0;
+    private LineRenderer pathRenderer;
+    private float audioForce = 0;
+    private List<float> spectrumValues = new List<float>();
+    private int spectrumSamplesCount;
+    private List<Vector3> pathPointsList = new List<Vector3>();
+
+    public void setAudioForce(float audioForce) {
+        this.audioForce = Mathf.Clamp(Mathf.Pow(audioForce * audioForcePreExponentMultiplier, 
+            audioForceExponent) * audioForcePostExponentMultipiler, 0, 0.95f);
+        text.text = "Force: " + audioForce;
+    }
+
+    public void setSpectrumValues(List<float> spectrumValues) {
+        this.spectrumValues = spectrumValues;
+    }
+
+    void Start() {
+        if (camera != null) {
+            cameraOffset = camera.transform.position - transform.position;
+        }
+
+        int maxPathPointCount = 2850;
+        for (int i = 0; i < maxPathPointCount; i++) {
+            pathPointsList.Add(new Vector3(deferentRadius + epicycleRadius, 0, 0));
+        }
+
+        pathRenderer = GetComponent<LineRenderer>();
+    }
+
+    float totalDotDeferentTravel = 0;
+    float totalDotEpicycleTravel = 0;
+    float remainingDotTravelThisFrame = 0;
+    void Update() {
+        colour.a = 0;
+        Color endColor = colour;
+        endColor.a = 0.94f;
+        pathRenderer.SetColors(colour, endColor);
+
+        totalDeferentTravel += deferentSpeed * audioForce;
+        totalEpicycleTravel += epicycleSpeed * audioForce;
+        float deferentCircumference = 2 * Mathf.PI * deferentRadius;
+        float deferentPercent = totalDeferentTravel / deferentCircumference;
+        float epicycleCircumference = 2 * Mathf.PI * epicycleRadius;
+        float epicyclePercent = totalEpicycleTravel / epicycleCircumference;
+        float deferentAngle = deferentPercent * Mathf.PI * 2;
+        float epicycleAngle = epicyclePercent * Mathf.PI * 2;
+        deferentRenderer.SetColors(Color.magenta, Color.cyan);
+        float dotSeparation = Mathf.PI / 20.0f;
+
+        remainingDotTravelThisFrame += audioForce;
+        while (remainingDotTravelThisFrame >= dotSeparation) {
+            float discreteDeferentAngle = (totalDotDeferentTravel/deferentCircumference) * Mathf.PI * 2;
+            float discreteEpicycleAngle = (totalDotEpicycleTravel/epicycleCircumference) * Mathf.PI * 2;
+
+            remainingDotTravelThisFrame -= dotSeparation;
+            totalDotDeferentTravel += deferentSpeed * dotSeparation;
+            totalDotEpicycleTravel += epicycleSpeed * dotSeparation;
+
+            Vector3 point = pathPointsList[0];
+            pathPointsList.RemoveAt(0);
+            point.x = deferentRadius * Mathf.Cos(discreteDeferentAngle) + epicycleRadius * Mathf.Cos(discreteEpicycleAngle);
+            point.z = deferentRadius * Mathf.Sin(discreteDeferentAngle) + epicycleRadius * Mathf.Sin(discreteEpicycleAngle);
+            pathPointsList.Add(point);
+        }
+
+        Vector3 deferentPosition = new Vector3(
+               deferentRadius * Mathf.Cos(deferentAngle),
+               0,
+               deferentRadius * Mathf.Sin(deferentAngle)
+        );
+
+        Vector3 epicyclePosition = new Vector3(
+            epicycleRadius * Mathf.Cos(epicycleAngle),
+            0,
+            epicycleRadius * Mathf.Sin(epicycleAngle)
+        );
+
+        Vector3 newPosition = deferentPosition + epicyclePosition;
+        transform.position = newPosition;
+
+        for (int reverseIndex = pathPointsList.Count - 1; reverseIndex >= 0; reverseIndex--) {
+            int forwardIndex = pathPointsList.Count - reverseIndex - 1;
+            Vector3 pathPoint = pathPointsList[reverseIndex];
+            if (forwardIndex < spectrumValues.Count) {
+                float spectrumValue = spectrumValues[forwardIndex];
+                float adjustedValue = Mathf.Pow(spectrumValue * visualPreExponentMultiplier, visualExponent);
+                pathPoint.y = adjustedValue;
+            } else {
+                pathPoint.y = 0;
+            }
+            pathPointsList[reverseIndex] = pathPoint;
+        }
+
+        pathRenderer.SetVertexCount(pathPointsList.Count);
+        pathRenderer.SetPositions((Vector3[]) pathPointsList.ToArray());
+
+        if (camera != null) {
+            camera.transform.position = deferentPosition + cameraOffset;
+        }
+
+        //drawDeferentAndEpicyclePaths(deferentPosition);
+    }
+
+    private void drawDeferentAndEpicyclePaths(Vector3 deferentPosition) {
+        int max = 180;
+        Vector3[] deferentPoints = new Vector3[max + 1];
+        Vector3[] epicyclePoints = new Vector3[max + 1];
+        for (int i = 0; i < max; i++) {
+            float angle = 2 * Mathf.PI * (i / (float) max);
+
+            Vector3 deferentPoint = new Vector3(
+                deferentRadius * Mathf.Cos(angle),
+                0,
+                deferentRadius * Mathf.Sin(angle)
+            );
+
+            Vector3 epicyclePoint = new Vector3(
+                epicycleRadius * Mathf.Cos(angle),
+                0,
+                epicycleRadius * Mathf.Sin(angle)
+            );
+            epicyclePoint += deferentPosition;
+
+            deferentPoints[i] = deferentPoint;
+            epicyclePoints[i] = epicyclePoint;
+        }
+
+        // Closes the polygons
+        epicyclePoints[max] = epicyclePoints[0];
+        deferentPoints[max] = deferentPoints[0];
+
+        epicycleRenderer.SetVertexCount(epicyclePoints.Length);
+        epicycleRenderer.SetPositions(epicyclePoints);
+
+        deferentRenderer.SetVertexCount(deferentPoints.Length);
+        deferentRenderer.SetPositions(deferentPoints);
+    }
+}
